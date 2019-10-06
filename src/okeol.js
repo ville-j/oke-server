2aefe9eb59fae65f9ec70f13986ab20f29d11371
Ville Jänkälä <ville.jankala@gmail.com>
2019-10-06 14:12:00 +0300

    Update auth routines

--- src/okeol.js
+++ src/okeol.js
@@ -1,4 +1,36 @@
 const db = require("./db");
+const crypto = require("crypto");
+
+const createRandomString = length => {
+  let string = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    string += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return string;
+};
+
+const createPasswordHash = (password, salt) => {
+  const preSaltedPass = crypto
+    .createHash("sha256")
+    .update(`${process.env.PWD_PRE_SALT}${password}`)
+    .digest("hex")
+    .toUpperCase();
+
+  return crypto
+    .createHash("sha256")
+    .update(
+      crypto
+        .createHash("sha256")
+        .update(`${salt}${preSaltedPass}`)
+        .digest("hex")
+    )
+    .digest("hex")
+    .toUpperCase();
+};
 
 const error = ({ code, field, text }) => {
   return {
@@ -32,9 +64,13 @@ const createUser = async ({ name, password }) => {
         text: "Password must be defined"
       });
     }
+
+    const salt = createRandomString(8);
+    const pwdhash = createPasswordHash(password, salt);
+
     const res = await db.query(
-      "INSERT INTO kuski (name, pwd, created) VALUES($1, $2, NOW()) RETURNING id, name, created",
-      [name, password]
+      "INSERT INTO kuski (name, pwdhash, pwdsalt, created) VALUES($1, $2, $3, NOW()) RETURNING id, name, created",
+      [name, pwdhash, salt]
     );
     return ok(res.rows[0]);
   } catch (e) {
@@ -64,11 +100,21 @@ const getUser = async ({ name, id }) => {
 };
 
 const auth = async ({ name, password }) => {
-  const res = await db.query(
-    "SELECT id, name, created FROM kuski WHERE name = $1 AND pwd = $2",
-    [name, password]
+  const pwdData = await db.query(
+    "SELECT pwdhash, pwdsalt FROM kuski WHERE name = $1",
+    [name]
   );
-  return ok(res.rows[0]);
+
+  if (pwdData.rows.length < 1)
+    return error({ code: 1, field: "name", text: "User does not exist" });
+
+  if (
+    createPasswordHash(password, pwdData.rows[0].pwdsalt) !==
+    pwdData.rows[0].pwdhash
+  )
+    return error({ code: 2, field: "password", text: "Wrong password" });
+
+  return getUser({ name });
 };
 
 module.exports = { createUser, getUsers, getUser, auth };
