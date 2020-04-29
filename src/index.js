f6bc3f27976e47eaa51c2322b3964e43c4e86c8d
Ville Jänkälä <ville.jankala@gmail.com>
2020-04-29 15:29:42 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const app = express();
 const port = 6543;
 const fs = require("fs");
 const API = require("./api");
-const path = require("path");
 const OkeApp = require("./okeol");
 const cmd = require("node-cmd");
 
@@ -68,7 +67,7 @@ app.post("/kuskis", async (req, res) => {
   const { name, password } = req.body;
   const result = await OkeApp.createKuski({
     name,
-    password
+    password,
   });
 
   if (result.ok) {
@@ -85,7 +84,7 @@ app.post("/auth", async (req, res) => {
   if (!user.data) res.sendStatus(401);
   else {
     const token = jwt.sign({ ...user.data }, process.env.JWT_SECRET, {
-      expiresIn: 86400
+      expiresIn: 86400,
     });
     res.json({ token });
   }
@@ -120,7 +119,7 @@ app.post("/kuskis/:name/shirt", async (req, res) => {
     form.maxFileSize = 4 * 1024 * 1024;
     form
       .parse(req)
-      .on("file", function(name, file) {
+      .on("file", function (name, file) {
         try {
           const shirt_tga = `${process.env.SHIRT_UPLOAD_PATH}${user.data.name}.tga`;
           const shirt_png = `${process.env.SHIRT_UPLOAD_PATH}${user.data.name}.png`;
@@ -136,12 +135,12 @@ app.post("/kuskis/:name/shirt", async (req, res) => {
                       OkeApp.setKuskiShirt({
                         id: user.data.id,
                         data: shirtData,
-                        crc: shirtCrcLE
+                        crc: shirtCrcLE,
                       });
-                      fs.unlink(shirt_tga, err => {
+                      fs.unlink(shirt_tga, (err) => {
                         err && console.log(err);
                       });
-                      fs.unlink(shirt_crc, err => {
+                      fs.unlink(shirt_crc, (err) => {
                         err && console.log(err);
                       });
                       res.sendStatus(201);
@@ -162,7 +161,7 @@ app.post("/kuskis/:name/shirt", async (req, res) => {
           res.sendStatus(500);
         }
       })
-      .on("error", err => {
+      .on("error", (err) => {
         console.log(err);
         res.sendStatus(500);
       });
@@ -235,14 +234,14 @@ app.get("/search", async (req, res) => {
   if (!query || query.length < 1) res.sendStatus(400);
   else {
     const kuskis = await OkeApp.searchKuskis({
-      query
+      query,
     });
     const levels = await OkeApp.searchLevels({
-      query
+      query,
     });
     res.json({
       kuskis: kuskis.data,
-      levels: levels.data
+      levels: levels.data,
     });
   }
 });
